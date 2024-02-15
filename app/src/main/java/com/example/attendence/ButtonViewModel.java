ae995ed0f0d71923ed120db2dbc40b1995242b27
venuvardhan22 <venuvardhandommeti@gmail.com>
2024-02-15 15:04:05 +0530

    neekundhi raa rey

--- app/src/main/java/com/example/attendence/ButtonViewModel.java
+++ app/src/main/java/com/example/attendence/ButtonViewModel.java
@@ -7,10 +7,12 @@ import java.util.List;
 public class ButtonViewModel extends ViewModel {
     private List<String> buttonData = new ArrayList<>();
 
+    // Method to add button data
     public void addButton(String buttonText) {
         buttonData.add(buttonText);
     }
 
+    // Method to get button data
     public List<String> getButtonData() {
         return buttonData;
     }
